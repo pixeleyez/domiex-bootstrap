ccc149a8e6501957ddfe8a8f6cd766f206e4b62f
Pooja-Pixeleyez <pooja@pixeleyez.com>
2024-11-22 18:25:10 +0530

    hospital staff - amangment

---
+++ src/assets/js/apps/hospital/staff/create-leave.init.js
@@ -0,0 +1,10 @@
+VirtualSelect.init({
+    ele: "#leaveTypeSelect",
+    options: [
+        { label: "Casual Leave", value: "casual" },
+        { label: "Sick Leave", value: "sick" },
+        { label: "Maternity Leave", value: "maternity" },
+        { label: "Emergency Leave", value: "emergency" },
+        { label: "Vacation Leave", value: "vacation" },
+    ],
+});
