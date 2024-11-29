9a58b70541ddb9348116c06da512313f491dddcf
Pooja-Pixeleyez <pooja@pixeleyez.com>
2024-11-29 18:16:38 +0530

    E-Commerece - Product Add, List, Grid View

---
+++ src/assets/js/apps/school/teachers/teachers-list.init.js
@@ -0,0 +1,14 @@
+//title Select
+VirtualSelect.init({
+    ele: "#titleSelect",
+    options: [
+        { label: "Teacher", value: "Teacher" },
+        { label: "Professor", value: "Professor" },
+        { label: "Assistant", value: "Assistant" },
+        { label: "Lecturer", value: "Lecturer" },
+        { label: "Instructor", value: "Instructor" },
+        { label: "Senior Lecturer", value: "Senior Lecturer" },
+        { label: "Associate Professor", value: "Associate Professor" },
+        { label: "Assistant Professor", value: "Assistant Professor" }
+    ],
+});
